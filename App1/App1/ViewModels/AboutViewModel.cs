10db455bc0d324357423be7cb9d76026a1f72025
Diego Tellez <alx.diego@gmail.com>
2023-02-27 22:35:49 -0500

    ajustes diseño

--- App1/App1/ViewModels/AboutViewModel.cs
+++ App1/App1/ViewModels/AboutViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App1.Views;
+using System;
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
@@ -9,10 +10,16 @@ namespace App1.ViewModels
     {
         public AboutViewModel()
         {
-            Title = "About";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            Title = "Inicio";
+            //Command = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            StartCommand = new Command(OnStartClick);
         }
 
-        public ICommand OpenWebCommand { get; }
+        public async void OnStartClick(object obj)
+        {
+            await Shell.Current.GoToAsync($"//{nameof(ItemsPage)}");
+        }
+
+        public Command StartCommand { get; }
     }
 }
--- App1/App1/ViewModels/LoginViewModel.cs
+++ App1/App1/ViewModels/LoginViewModel.cs
@@ -2,7 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Essentials;
 using Xamarin.Forms;
+
 
 namespace App1.ViewModels
 {
